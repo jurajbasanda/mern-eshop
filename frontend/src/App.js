b759f204de4f5ab01b071a3aef72c66e9279fd04
Juraj Basanda <basanda.juraj@gmail.com>
2020-10-06 21:30:34 +0100

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+//Components
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './components/screens/HomeScreen'
+import ProductScreen from './components/screens/ProductScreen'
+import CartScreen from './components/screens/CartScreen'
+
+//Style
+import { Container } from 'react-bootstrap'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" exact>
+            <HomeScreen />
+          </Route>
+          <Route path="/product/:id" component={ProductScreen}>
+            <ProductScreen />
+          </Route>
+          <Route path="/cart">
+            <CartScreen />{' '}
+          </Route>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
