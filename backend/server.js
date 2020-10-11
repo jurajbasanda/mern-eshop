a6fff2d22a177bd11b0494ca83f3669ea309e16a
Juraj Basanda <basanda.juraj@gmail.com>
2020-10-11 19:56:50 +0100

    demo deploy

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-import express from "express"
-import dotenv from "dotenv"
-import connectDB from "./config/db.js"
-import productRoutes from "./routes/productRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -13,16 +13,23 @@ const app = express()
 app.use(express.json())
 
 //Router
-app.get(`/`, (req, res) => res.send("API is running !"))
-app.use("/api/products", productRoutes)
-app.use("/api/users", userRoutes)
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+//Deploy
+const __dirname = path.resove()
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(pathjoin(__dirname, '/frontend/build')))
+
+	app.get('*', (req, res) => res.sendFile(path.resolve((__dirname, 'frontend', 'build', 'index.html'))))
+}else{
+	app.get('/',(req,res)=>{
+		res.send('API IS RUNNING')
+	})
+}
 //Errors
 app.use(notFound)
 app.use(errorHandler)
-//Set PORT 
+//Set PORT
 const PORT = process.env.PORT || 5000
 //Start server
-app.listen(
-	PORT,
-	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-)
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
---
+++ frontend/build/precache-manifest.82b92605a5d9b43aa75d1cfcec2c261b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf610589b57483c2f8c9dab5b583346",
+    "url": "/index.html"
+  },
+  {
+    "revision": "014ddc237dcbf8664d47",
+    "url": "/static/css/main.a1c40666.chunk.css"
+  },
+  {
+    "revision": "0276f1468e7befb97f67",
+    "url": "/static/js/2.fa080905.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fa080905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "014ddc237dcbf8664d47",
+    "url": "/static/js/main.2c7bb474.chunk.js"
+  },
+  {
+    "revision": "9da0f37ed9af22ebcf86",
+    "url": "/static/js/runtime-main.cea7d395.js"
+  }
+]);
