c5da54205a46090d8f8dcc045192a4991bd29f4f
Juraj Basanda <basanda.juraj@gmail.com>
2020-10-28 20:08:28 +0000

    make it better

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-
+import * as path from 'path'
 dotenv.config()
 
 connectDB()
@@ -22,6 +22,14 @@ app.get('/api/config/paypal', (req, res) =>
 ) //Errors
 app.use(notFound)
 app.use(errorHandler)
+//Check if it's in production
+if(process.env.NODE_ENV === 'production'){
+//Set static folder
+app.use(express.static('../frontend/build'))
+app.get('*',(req,res)=>{
+res.sendFile(path.resolve(__dirname,'../frontend','build','index.html'))
+})
+}
 //Set PORT
 const PORT = process.env.PORT || 5000
 //Start server
