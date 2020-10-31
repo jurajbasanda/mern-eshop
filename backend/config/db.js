ba5e0491e49272c81468592310c18fb6b9837a5e
Juraj Basanda <basanda.juraj@gmail.com>
2020-10-31 20:22:56 +0000

    deploy

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose'
+import colors from 'colors'
 
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true
         })
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
+        console.log(`MongoDB Connected: ${conn.connection.host}`.blue.bold)
     }
     catch (error) {
         console.log(`Error: ${error.messege}`)
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
+import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-import * as path from 'path'
+
 dotenv.config()
 
 connectDB()
@@ -19,21 +21,30 @@ app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.get('/api/config/paypal', (req, res) =>
 	res.send(process.env.PAYPAL_CLIENT_ID)
-) //Errors
+) 
+const __dirname = path.resolve()
+//Check if it's in production
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, './frontend/build/')))
+
+	app.get('/', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+	)
+} else {
+	app.get('/', (req, res) => {
+		res.send('API is running....')
+	})
+}
+
+//Errors
 app.use(notFound)
 app.use(errorHandler)
-//Check if it's in production
-if(process.env.NODE_ENV === 'production'){
-//Set static folder
-app.use(express.static('../frontend/build'))
-app.get('*',(req,res)=>{
-res.sendFile(path.resolve(__dirname,'../frontend','build','index.html'))
-})
-}
-//Set PORT
+
 const PORT = process.env.PORT || 5000
-//Start server
 app.listen(
 	PORT,
-	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold
+	)
 )
