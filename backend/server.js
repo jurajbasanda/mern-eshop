c7ec2336cc8e2ed3def490bf128aec90032bde19
Juraj Basanda <basanda.juraj@gmail.com>
2020-11-01 18:42:09 +0000

    upload  new product image

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
@@ -25,12 +26,16 @@ app.use(express.json())
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
+
 app.get('/api/config/paypal', (req, res) =>
 	res.send(process.env.PAYPAL_CLIENT_ID)
 ) 
 const __dirname = path.resolve()
-//Check if it's in production
+//Make uploads static folder
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+//Check if it's in production
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, './frontend/build/')))
 
